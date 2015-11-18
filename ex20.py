3c1bcfeb23f1f2a3c13db9e13b461a0852afc5cb
4bic <robyne.kiplangat@gmail.com>
2015-11-18 08:46:58 +0300

    functions

--- ex20.py
+++ ex20.py
@@ -6,9 +6,9 @@ def print_all(f):
 	print f.read()
 
 def rewind(f):
-	f.seek
+	f.seek(0)
 
-def print_a_line(line_count):
+def print_a_line(line_count, f):
 	print line_count, f.readline()  
 
 current_file = open(input_file)	
@@ -24,6 +24,7 @@ rewind(current_file)
 print "Lets print three lines:"
 
 current_line = 1
+
 print_a_line(current_line, current_file)
 
 current_line = current_line + 1
