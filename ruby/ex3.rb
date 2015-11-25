8dc04c6960379cb402b7f01384d77f531f8b1260
robin kiplangat <4bic@users.noreply.github.com>
2015-11-26 00:24:36 +0300

    Revert "Lpthw"

---
+++ ruby/ex3.rb
@@ -0,0 +1,25 @@
+# outputs what is held under the double quotes
+puts "I will now count my chickens:"
+#outputs the value of the object under the double quotes then does the maths and outputs the value
+puts "Hens",25.0 + 30/6
+#as line 2
+puts "Roosters", 100.00 - 25.00 * 3 % 4
+#outputs whatever is held under the double quotes
+puts "Now I will count the eggs:"
+#outputs the value of the arithmetic problem
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#considers as a logic question
+puts "is it true that 3 + 2 < 5 - 7?"
+#outputs objects under double quotes
+puts 3.0 +  2.0 < 5.0 - 7.98
+# outputs value of math
+puts "What is 3 + 2?", 3 + 2.00
+puts "What is 5 - 7?", 5 - 7.0
+
+puts "oh that's why its false."
+
+puts "How about some more."
+#logical qn
+puts "Is it greater?", 5 > -2
+puts "Is it greater or equal?", 5 >= -2
+puts "Is it less or equal?", 5 <= -2
