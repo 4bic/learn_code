2529ffffd28c59315c63f16dbfb39655164e59eb
4bic <robyne.kiplangat@gmail.com>
2016-08-04 14:14:47 +0300

    clean up

--- ruby/ex1.rb
+++
@@ -1,7 +0,0 @@
-puts "Hello World !"
-puts "Hello Again"
-puts "i like typing this."
-puts "this is fun."
-puts 'Yay! Printing.'
-puts "i'd much rather you 'not' ."
-puts 'I "said" do not touch this.'
