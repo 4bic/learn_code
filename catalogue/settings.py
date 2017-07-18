9aa16941db558cfc099c56fa1c469685b84909bb
4bic <robyne.kiplangat@gmail.com>
2017-07-18 15:42:43 +0300

    clarify db

--- catalogue/settings.py
+++ catalogue/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'catalogue.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'catalogue.db'),
     }
 }
 
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-GB'
 
 TIME_ZONE = 'UTC'
 
