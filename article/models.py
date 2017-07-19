47f4e0c3d34dc9325a7909fd009aeaf909790543
4bic <robyne.kiplangat@gmail.com>
2017-07-19 07:32:33 +0300

    Articles Model

--- article/models.py
+++ article/models.py
@@ -4,3 +4,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    """docstring for Article."""
+    title = models.CharField(max_length=200)
+    body = models.TextField()
+    pub_date = models.DateTimeField('date published')
+    likes = models.IntegerField()
