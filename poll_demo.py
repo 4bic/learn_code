7d7510f4ab5f305aa07e38c0c145af69eb91ac5b
4bic <robyne.kiplangat@gmail.com>
2017-08-24 12:58:01 +0300

    read / write cvsv with pandas

---
+++ poll_demo.py
@@ -0,0 +1,18 @@
+import sys
+from pandas import *
+import pandas as pd
+import numpy as np
+import csv
+if __name__ == '__main__':
+    '''create pandas dataframe '''
+
+    # read csv
+    mca_data =  pandas.read_csv('Kenya_MCA_2017.csv')
+    #create dataframe
+    mca_df = pandas.DataFrame(mca_data)
+    # combine surname and other name into Full Name
+    mca_data['fullName'] = mca_data['Surname'] + ' ' + mca_data['Other_Names']
+    # write to csv
+    mca_data.to_csv('mca_data_with_fullName.csv')
+
+    # print mca_df[mca_df['Ward_Code'] <= 50]
