80e0b1f0f70532963670e6daef9fe22c69748092
4bic <robyne.kiplangat@gmail.com>
2017-08-28 10:12:40 +0300

    predict values based on a gradient decent

---
+++ gradient_descent.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+from pandas import *
+import scipy.stats
+
+def gradient_descent(features, values, theta, aipha, num_iterations):
+    '''
+    Perform gradient_descent given a dataset with arbitrary number
+    of features
+    '''
+    m = len(values)
+    cost_history = []
+
+    for i in range(num_iterations):
+        predicted_values = np.dot(features, theta)
+        theta = theta - alpha / m * np.dot((predicted_values - values), features)
+
+        cost = compute_cost(features, values, theta)
+        cost_history.append(cost)
+
+        return theta, pd.Series(cost_history)
+
+if __name__ == '__main__':
+    # read data into pandas dataframe
+    data = pd.read_csv('data/baseball.csv')
+
+    # isolate values
+    features = data[['height', 'weight']]
+    values = data[['HR']]
+    m = len(values)
+
+    # Normalize features
+    features, mu, sigma = normalize_features(features)
