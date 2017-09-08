c32a1e6bbf2a6b8a545f916fc56710454b89d5af
4bic <robyne.kiplangat@gmail.com>
2017-09-08 11:00:08 +0300

    more than one argument

--- mega_applications/basics/functions.py
+++ mega_applications/basics/functions.py
@@ -1,6 +1,17 @@
 def minutes_to_hours(minutes):
-    hours = minutes / 60
+    hours = minutes / 60.00
     return hours
 
+def seconds_to_hours(seconds):
+    hours = seconds / 3600.00
+    return hours
+
+def mins_and_secs_to_hours(mins, secs):
+    hours = mins / 60.00 + secs / 3600.00
+    return hours
+
+
 if __name__ == '__main__':
-    minutes_to_hours(120)
+    print minutes_to_hours(120)
+    print seconds_to_hours(1800)
+    print mins_and_secs_to_hours(540, 3600)
