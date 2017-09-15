cc8f5dae3b2102040d563750a782286029065fa8
4bic <robyne.kiplangat@gmail.com>
2017-09-15 13:06:53 +0300

    check hosts file for website domain redirect

--- mega_applications/web_block/site_blocker.py
+++ mega_applications/web_block/site_blocker.py
@@ -2,7 +2,8 @@ import time
 from datetime import datetime as dt
 
 # determine where the host file is located
-hosts_path = "/etc/hosts"
+hosts_temp = 'hosts'
+#hosts_path =  "/etc/hosts"
 # prefered url to redirect from distracting sites
 redirect = "127.0.0.1"
 # sites to be blocked from
@@ -11,6 +12,12 @@ website_list = ["www.facebook.com", "www.twitter.com", "mail.google.com"]
 while True:
     if dt(dt.now().year,dt.now().month,dt.now().day,8)< dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
         print("Working hours ....")
+        with open(hosts_temp, 'r+') as file:
+            content = file.read()
+            for website in website_list:
+                if website in content:
+                    pass
+                    
     else:
         print ("Fun hour . .('_')")
     time.sleep(5)
