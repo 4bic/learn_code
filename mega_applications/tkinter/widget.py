cda174d68a04c40bbf1471c9d5b8932c9bff8ff9
4bic <robyne.kiplangat@gmail.com>
2017-09-15 16:45:42 +0300

    test with a function

--- mega_applications/tkinter/widget.py
+++ mega_applications/tkinter/widget.py
@@ -3,14 +3,19 @@ from Tkinter import *
 # create a window
 window = Tk()
 
-# create widgets
-# button
-b1 = Button(window, text="Execute")
+def km_to_miles():
+    print("Success!")
+
+# create widget button
+b1 = Button(window, text="Execute", command=km_to_miles)
 b1.grid(row=0, column=0, rowspan=2)
 
+# entry panel
 e1=Entry(window)
 e1.grid(row=0, column=1)
 
+# text field
 t1 =Text(window,height=1, width=20)
 t1.grid(row=0, column=2)
+
 window.mainloop()
