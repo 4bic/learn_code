e0aad7e6b22cf81a09ffdea2c0abd3853cf22f87
4bic <robyne.kiplangat@gmail.com>
2017-09-18 12:58:38 +0300

    add text variable to widget

--- mega_applications/tkinter/widget.py
+++ mega_applications/tkinter/widget.py
@@ -4,14 +4,16 @@ from Tkinter import *
 window = Tk()
 
 def km_to_miles():
-    print("Success!")
+    print(e1_value.get())
+    t1.insert(END, e1_value.get())
 
 # create widget button
 b1 = Button(window, text="Execute", command=km_to_miles)
 b1.grid(row=0, column=0, rowspan=2)
 
 # entry panel
-e1=Entry(window)
+e1_value=StringVar()
+e1=Entry(window, textvariable=e1_value)
 e1.grid(row=0, column=1)
 
 # text field
