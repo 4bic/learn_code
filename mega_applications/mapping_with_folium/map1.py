ffcf85131bbd5022b1b2a7c5120bac4ad47d84f4
4bic <robyne.kiplangat@gmail.com>
2017-09-21 13:18:44 +0300

    popup window with dynamc info

--- mega_applications/mapping_with_folium/map1.py
+++ mega_applications/mapping_with_folium/map1.py
@@ -5,7 +5,8 @@ import pandas
 volcano_data = pandas.read_csv("Volcanoes.txt")
 latitude = list(volcano_data["LAT"])
 longitude = list(volcano_data["LON"])
-#marker_name = volcano_data["LOCATION"]
+elevation = list(volcano_data["ELEV"])
+marker_name = volcano_data["LOCATION"]
 
 # create base map
 map = folium.Map(location=[ 38.58, -99.09], zoom_start=12, tiles="Mapbox Bright")
@@ -13,9 +14,9 @@ map = folium.Map(location=[ 38.58, -99.09], zoom_start=12, tiles="Mapbox Bright"
 fg = folium.FeatureGroup(name="My Map")
 
 # iterate over the data file and use location data
-for lt, ln in zip(latitude,longitude):
+for lt, ln, el,nm in zip(latitude,longitude, elevation, marker_name):
     # add a each marker
-    fg.add_child(folium.Marker(location=[lt,ln], popup="Volcano Marker", icon=folium.Icon(color='green')))
+    fg.add_child(folium.Marker(location=[lt,ln], popup=str(nm)+ "-"+str(el), icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 # save genarated map
