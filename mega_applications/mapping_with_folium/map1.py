da17833ee6e89a3b51e0d961ec99e3dd068f6970
4bic <robyne.kiplangat@gmail.com>
2017-09-21 13:06:09 +0300

    iterate over datafile and extract location data

--- mega_applications/mapping_with_folium/map1.py
+++ mega_applications/mapping_with_folium/map1.py
@@ -1,6 +1,22 @@
 import folium
-# create map
-map = folium.Map(location=[ -1.285834, 36.728103], zoom_start=6, tiles="Mapbox Bright")
-map.add_child(folium.Marker(location=[-1.29, 36.73], popup="Hi, this is my Home Marker", icon=folium.Icon(color='green')))
+import pandas
+
+# variables
+volcano_data = pandas.read_csv("Volcanoes.txt")
+latitude = list(volcano_data["LAT"])
+longitude = list(volcano_data["LON"])
+#marker_name = volcano_data["LOCATION"]
+
+# create base map
+map = folium.Map(location=[ 38.58, -99.09], zoom_start=12, tiles="Mapbox Bright")
+# utilize a feature group object to add multiple features
+fg = folium.FeatureGroup(name="My Map")
+
+# iterate over the data file and use location data
+for lt, ln in zip(latitude,longitude):
+    # add a each marker
+    fg.add_child(folium.Marker(location=[lt,ln], popup="Volcano Marker", icon=folium.Icon(color='green')))
+
+map.add_child(fg)
 # save genarated map
-map.save("Nairobi.html")
+map.save("US_VOLCANOES.html")
