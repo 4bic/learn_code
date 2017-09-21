04e9ba39987e6198601e84240477b3d0e3f1eea5
4bic <robyne.kiplangat@gmail.com>
2017-09-21 12:09:16 +0300

    add a point marker

--- mega_applications/mapping_with_folium/map1.py
+++ mega_applications/mapping_with_folium/map1.py
@@ -1,5 +1,6 @@
 import folium
-
-map = folium.Map(location=[ 36.728103, -1.285834], zoom_start=6, tiles="Mapbox Bright")
-
+# create map
+map = folium.Map(location=[ -1.285834, 36.728103], zoom_start=6, tiles="Mapbox Bright")
+map.add_child(folium.Marker(location=[-1.29, 36.73], popup="Hi, this is my Home Marker", icon=folium.Icon(color='green')))
+# save genarated map
 map.save("Nairobi.html")
