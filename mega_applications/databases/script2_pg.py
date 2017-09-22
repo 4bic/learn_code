554f4ea98cd167450a333a6540c606bcd5fea298
4bic <robyne.kiplangat@gmail.com>
2017-09-22 12:28:16 +0300

    working on postgres access

---
+++ mega_applications/databases/script2_pg.py
@@ -0,0 +1,49 @@
+import psycopg2
+
+def create_table():
+    # coonect to db
+    conn = psycopg2.connect(dbname='mega_app', user='4bic',password='postgres', host='localhost', port='5432')
+    # create a cursor object
+    cur=conn.cursor()
+    # execute db creation
+    cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
+    # commit to db
+    conn.commit()
+    conn.close()
+
+def insert(item,quantity,price):
+    # connect to db
+    conn = psycopg2.connect(dbname='mega_app', user='4bic',password='postgres', host='localhost', port='5432')
+    # create a cursor object
+    cur=conn.cursor()
+    # add data
+    cur.execute("INSERT INTO store VALUES('%s','%s','%s')" %(item,quantity,price))
+    # commit to db
+    conn.commit()
+    conn.close()
+
+def view():
+    conn = psycopg2.connect(dbname='mega_app', user='4bic',password='postgres', host='localhost', port='5432')
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM store")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+# def delete(item):
+#     conn = psycopg2.connect("mega_app")
+#     cur=conn.cursor()
+#     cur.execute("DELETE FROM store WHERE item=?",(item,))
+#     conn.commit()
+#     conn.close()
+#
+# def update(quantity,price,item):
+#     conn = psycopg2.connect("mega_app")
+#     cur=conn.cursor()
+#     cur.execute("UPDATE store SET quantity=?, price=? WHERE item=?",(quantity,price,item))
+#     conn.commit()
+#     conn.close()
+
+create_table()
+insert("Wheat", 50, 30)
+# print (view())
