b0eb178b027417593de629d2c763685d8c67bd01
4bic <robyne.kiplangat@gmail.com>
2017-09-25 08:04:09 +0300

    attach scrollbar to the Listbox

--- mega_applications/bookstore/bookstore.py
+++ mega_applications/bookstore/bookstore.py
@@ -31,6 +31,17 @@ isbn_text=StringVar()
 e4=Entry(window, textvariable=isbn_text)
 e4.grid(row=1, column=3)
 
+# Listbox
+list1=Listbox(window, height=6,width=35)
+list1.grid(row=3,column=0,rowspan=6,columnspan=2)
+# scrollbar
+sb1=Scrollbar(window)
+sb1.grid(row=2,column=2,rowspan=6)
+
+# attach scrollbar to the Listbox
+list1.configure(yscrollcommand=sb1.set)
+sb1.configure(command=list1.yview)
+
 
 
 window.mainloop()
