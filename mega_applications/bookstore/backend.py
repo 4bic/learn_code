bc83417d1d467449c1d84e0b105608428fec1498
4bic <robyne.kiplangat@gmail.com>
2017-09-25 09:03:10 +0300

    search,delete and update functions

--- mega_applications/bookstore/backend.py
+++ mega_applications/bookstore/backend.py
@@ -22,6 +22,27 @@ def view():
     conn.close()
     return rows
 
+def search(title="",author="",year="",isbn=""):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?",(title,author,year,isbn))
+    rows=cur.fetchall()
+    conn.close()
+    return rows
 
+def delete(id):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("DELETE FROM book  WHERE id=?",(id,))
+    conn.commit()
+    conn.close()
 
+def update(id,title,author,year,isbn):
+    conn=sqlite3.connect("books.db")
+    cur=conn.cursor()
+    cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?",(title,author,year,isbn,id))
+    conn.commit()
+    conn.close()
+
+# connect to db everytime frontend is initiated    
 connect()
