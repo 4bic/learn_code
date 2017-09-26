6c00daa9f705f27f43aa5d989522c0b557c6ddb0
4bic <robyne.kiplangat@gmail.com>
2017-09-26 14:05:46 +0300

    saving alterations to account

--- mega_applications/bank/acc.py
+++ mega_applications/bank/acc.py
@@ -1,15 +1,24 @@
 class Account(object):
     """Description for Account class."""
     def __init__(self,filepath):
+        self.filepath = filepath
         with open(filepath, 'r') as file:
             self.balance = int(file.read())
 
-
+    # withraw from account
     def withdraw(self, amount):
         self.balance=self.balance - amount
-
+    # deposit to account
     def deposit(self, amount):
         self.balance = self.balance + amount
+    # save alterations to account
+    def commit(self):
+        with open(self.filepath, 'w') as file:
+            file.write(str(self.balance))
+
+
+
+
 
 
 
@@ -19,3 +28,4 @@ account.withdraw(150)
 print (account.balance)
 account.deposit(1150)
 print (account.balance)
+account.commit()
