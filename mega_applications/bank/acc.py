0ef6bbd4f8dd3139fea52370acff95570b6a5b31
4bic <robyne.kiplangat@gmail.com>
2017-09-26 13:54:00 +0300

    withdraw and deposit functions

--- mega_applications/bank/acc.py
+++ mega_applications/bank/acc.py
@@ -3,6 +3,19 @@ class Account(object):
     def __init__(self,filepath):
         with open(filepath, 'r') as file:
             self.balance = int(file.read())
-            
+
+
+    def withdraw(self, amount):
+        self.balance=self.balance - amount
+
+    def deposit(self, amount):
+        self.balance = self.balance + amount
+
+
 
 account=Account("balance.txt")
+print (account.balance)
+account.withdraw(150)
+print (account.balance)
+account.deposit(1150)
+print (account.balance)
