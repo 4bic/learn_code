5c86787cddec1ba89435f07ec97e69c7c0fe0fb2
4bic <robyne.kiplangat@gmail.com>
2017-09-26 05:43:50 +0300

    clean Class

--- mega_applications/oop_bookstore/backend.py
+++ mega_applications/oop_bookstore/backend.py
@@ -12,28 +12,23 @@ class Database:
     def insert(self,title,author,year,isbn):
         self.cur.execute("INSERT INTO book  VALUES (NULL,?,?,?,?)",(title,author,year,isbn))
         self.conn.commit()
-        self.conn.close()
 
     def view(self):
         self.cur.execute("SELECT * FROM book")
         rows=self.cur.fetchall()
-        self.conn.close()
         return rows
 
     def search(self,title="",author="",year="",isbn=""):
-        cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?",(title,author,year,isbn))
-        rows=cur.fetchall()
-        conn.close()
+        self.cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?",(title,author,year,isbn))
+        rows=self.cur.fetchall()
         return rows
 
     def delete(self,id):
-        cur.execute("DELETE FROM book  WHERE id=?",(id,))
-        conn.commit()
-        conn.close()
+        self.cur.execute("DELETE FROM book  WHERE id=?",(id,))
+        self.conn.commit()
 
     def update(self,id,title,author,year,isbn):
-        cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?",(title,author,year,isbn,id))
-        conn.commit()
-        conn.close()
+        self.cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?",(title,author,year,isbn,id))
+        self.conn.commit()
 
 # connect to db everytime frontend is initiated
