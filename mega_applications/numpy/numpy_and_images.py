c4fee7de66ccd1111ab9a0716d7492e471c79ff2
4bic <robyne.kiplangat@gmail.com>
2017-09-27 07:53:29 +0300

    slicing and iterating through a numpy array

--- mega_applications/numpy/numpy_and_images.py
+++ mega_applications/numpy/numpy_and_images.py
@@ -8,7 +8,23 @@ im_g = cv2.imread("smallgray.png", 1) #0-grayscale ; 1-BGR array
 # creating images from numpy arrays
 cv2.imwrite("newsmallgray.png",im_g)
 
-print im_g
+# print im_g
 # results for (0) :[[187 158 104 121 143]
 #              [198 125 255 255 147]
 #              [209 134 255  97 182]]
+
+# slicing an array
+# 3rd & 4th elements on first & 2nd rows
+im_g[0:2, 2:4]
+
+# check shape of array
+img.shape
+
+# iterating thro
+#1
+for i in im_g:
+    print (i)
+#2
+# value for value
+for i in im_g.flat:
+    print (i)
