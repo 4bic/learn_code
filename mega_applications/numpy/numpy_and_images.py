be7eff460af7739aa9fe9666a70fdf87d4bb096d
4bic <robyne.kiplangat@gmail.com>
2017-09-27 07:44:44 +0300

    creating numpy arrays from images

--- mega_applications/numpy/numpy_and_images.py
+++ mega_applications/numpy/numpy_and_images.py
@@ -1,6 +1,11 @@
 import numpy as np
 import pandas as pd
+import cv2
 
-n = np.arange(27)
+# creating numpy arrays out of images
+im_g = cv2.imread("smallgray.png", 1) #0-grayscale ; 1-BGR array
 
-print n.reshape(3, 3, 3)
+print im_g
+# results for (0) :[[187 158 104 121 143]
+#              [198 125 255 255 147]
+#              [209 134 255  97 182]]
