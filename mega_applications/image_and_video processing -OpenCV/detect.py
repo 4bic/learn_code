3d42465cb2adb46b690d973fd4f936a04e15f21b
4bic <robyne.kiplangat@gmail.com>
2017-09-28 06:35:06 +0300

    load and display image with opencv

---
+++ mega_applications/image_and_video processing -OpenCV/detect.py
@@ -0,0 +1,23 @@
+import cv2
+
+# read image
+#   0-grayscale, 1-BGR , -1 >color with transparency
+img = cv2.imread("galaxy.jpg", 0)
+
+# print (type(img))
+# <type 'numpy.ndarray'>
+# print img
+# [[14 18 14 ..., 20 15 16]
+#  [12 16 12 ..., 20 15 17]
+#  [12 13 16 ..., 14 24 21]
+#  ...,
+#  [ 0  0  0 ...,  5  8 14]
+#  [ 0  0  0 ...,  2  3  9]
+#  [ 1  1  1 ...,  1  1  3]]
+
+# show image on a window
+cv2.imshow('GALAXy', img)
+# time to close window
+cv2.waitKey(2000)
+# action to be taken once the window is clicked
+cv2.destroyAllWindows()
