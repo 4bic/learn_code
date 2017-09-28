2381bbf3e450b8be66c8fc9d217c46bfc99c8811
4bic <robyne.kiplangat@gmail.com>
2017-09-28 07:08:26 +0300

    describe acions points

--- mega_applications/image_and_video processing -OpenCV/image_resizer/resize.py
+++ mega_applications/image_and_video processing -OpenCV/image_resizer/resize.py
@@ -5,12 +5,18 @@ import glob
 images = glob.glob("*.jpg")
 
 def resize():
+    # iterate over folder for every image filw
     for image in images:
+        # read image size and shape
         img = cv2.imread(image, 0)
+        # resize image
         re_img = cv2.resize(img,(100,100))
+        # briefly display resized image on window
         cv2.imshow("hey", re_img)
         cv2.waitKey(500)
+        # kill the window
         cv2.destroyAllWindows()
+        # save resized image to file
         cv2.imwrite("resized_"+image, re_img)
 
 
