1db6cd03ba28e21c48c6e5a294cbd4a9b1b664a2
4bic <robyne.kiplangat@gmail.com>
2017-09-28 14:48:38 +0300

    faces detected with scaleFactor=1.05

--- mega_applications/image_and_video processing -OpenCV/face_detector.py
+++ mega_applications/image_and_video processing -OpenCV/face_detector.py
@@ -2,7 +2,10 @@ import cv2
 
 face_cascade = cv2.CascadeClassifier("resource/haarcascade_frontalface_default.xml")
 # open colored image
-img = cv2.imread("resource/photo.jpg")
+# img = cv2.imread("resource/photo.jpg")
+# dealing with a more faces within a photo
+img = cv2.imread("resource/news.jpg")
+
 # convert BGR image to grayscale
 gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -22,6 +25,6 @@ for x, y, w, h in faces:
 # resize incase of large images
 resized_img = cv2.resize(img, ((int(img.shape[1]/3)), (int(img.shape[0]/3))))
 cv2.imshow("Gray", resized_img)
-cv2.imwrite('resource/resized_face.png', resized_img)
+cv2.imwrite("resource/resized_news_02.jpg", resized_img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
