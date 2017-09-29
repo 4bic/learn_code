2075301aa754a74158eef9c3a17a86458d6575de
4bic <robyne.kiplangat@gmail.com>
2017-09-29 14:56:42 +0300

    capture no of frames

--- mega_applications/image_and_video processing -OpenCV/vid_capture.py
+++ mega_applications/image_and_video processing -OpenCV/vid_capture.py
@@ -2,9 +2,12 @@ import cv2,time
 
 # capture video
 video=cv2.VideoCapture(0)# 0-built in cam, 1-external cam
+a = 1
 while True:
+    a=a+1
     # create frame to read images captures by cam
     check, frame= video.read()
+    print frame
     # convert image to grayscale
     gray_img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     # check confirms video is running
@@ -16,7 +19,7 @@ while True:
 
     if key==ord('q'):
         break
-
+print a
 # release video.camera
 video.release()
 # destroy all open windows
