a5c8a811ed8003f9794e71dadc79893026c1faa7
4bic <robyne.kiplangat@gmail.com>
2017-09-29 14:41:44 +0300

    process video as image frames

--- mega_applications/image_and_video processing -OpenCV/vid_capture.py
+++ mega_applications/image_and_video processing -OpenCV/vid_capture.py
@@ -2,5 +2,20 @@ import cv2,time
 
 # capture video
 video=cv2.VideoCapture(0)# 0-built in cam, 1-external cam
+
+# create frame to read images captures by cam
+check, frame= video.read()
+# check confirms video is running
+# print check
+# TRUE
+
+time.sleep(3)
+# display window
+cv2.imshow("Video Capture", frame)
+
+# period window to stay active
+cv2.waitKey(0)
 # release video.camera
 video.release()
+# destroy all open windows
+cv2.destroyAllWindows()
