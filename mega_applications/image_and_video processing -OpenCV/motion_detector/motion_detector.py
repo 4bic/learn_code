fb53fc9cd58fa8b960e909c62f68607f1a07b1a0
4bic <robyne.kiplangat@gmail.com>
2017-10-02 06:49:11 +0300

    capture static images

---
+++ mega_applications/image_and_video processing -OpenCV/motion_detector/motion_detector.py
@@ -0,0 +1,24 @@
+import cv2,time
+
+# capture video
+video=cv2.VideoCapture(0)# 0-built in cam, 1-external cam
+
+while True:
+    # create frame to read images captures by cam
+    check, frame= video.read()
+    # convert image to grayscale
+    gray_img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    # check confirms video is running
+    # time.sleep(3)
+    # display window
+    cv2.imshow("Video Capturing", gray_img)
+    # period window to stay active
+    key = cv2.waitKey(1)
+
+    if key==ord('q'):
+        break
+
+# release video.camera
+video.release()
+# destroy all open windows
+cv2.destroyAllWindows()
