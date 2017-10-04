11aaca71fc44164d8893df61ca8bea6d2bf02746
4bic <robyne.kiplangat@gmail.com>
2017-10-04 13:22:42 +0300

    style to clean the graph

--- mega_applications/image_and_video processing -OpenCV/motion_detector/plotting.py
+++ mega_applications/image_and_video processing -OpenCV/motion_detector/plotting.py
@@ -3,9 +3,15 @@ from bokeh.plotting import figure, output_file, show
 
 # create figure object
 p=figure(x_axis_type='datetime', height=100, width=500, responsive=True, title="Motion Graph")
+# styling
+p.yaxis.minor_tick_line_color=None
+# remove ticks on yaxis
+p.ygrid[0].ticker.desired_num_ticks=1
 # plot a quadrant chart
 q = p.quad(left=df["Start"], right=df["End"],bottom=0, top=1, color="orange")
 
-output_file("graph.html",mode='inline')
+
+
+output_file("motion_graph.html",mode='inline')
 
 show(p)
