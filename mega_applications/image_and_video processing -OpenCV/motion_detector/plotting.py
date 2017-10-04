da7515a42de673c9a953428e18d44ee2cf213577
4bic <robyne.kiplangat@gmail.com>
2017-10-04 13:14:20 +0300

    sample motion graph from the webcam

--- mega_applications/image_and_video processing -OpenCV/motion_detector/plotting.py
+++ mega_applications/image_and_video processing -OpenCV/motion_detector/plotting.py
@@ -4,7 +4,7 @@ from bokeh.plotting import figure, output_file, show
 # create figure object
 p=figure(x_axis_type='datetime', height=100, width=500, responsive=True, title="Motion Graph")
 # plot a quadrant chart
-q = p.quad(left=df["Start"], right=df["End"],bottom=0, top=1, color="blue")
+q = p.quad(left=df["Start"], right=df["End"],bottom=0, top=1, color="orange")
 
 output_file("graph.html",mode='inline')
 
