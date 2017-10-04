e291e563acd6d028c30d398c1c6c043d32db6a28
4bic <robyne.kiplangat@gmail.com>
2017-10-04 13:34:03 +0300

    define data source

--- mega_applications/image_and_video processing -OpenCV/motion_detector/plotting.py
+++ mega_applications/image_and_video processing -OpenCV/motion_detector/plotting.py
@@ -1,6 +1,8 @@
 from motion_detector import df #import data
 from bokeh.plotting import figure, output_file, show
-from bokeh.models import HoverTool
+from bokeh.models import HoverTool, ColumnDataSource
+
+cds=ColumnDataSource(df)
 
 # create figure object
 p=figure(x_axis_type='datetime', height=100, width=500, responsive=True, title="Motion Graph")
@@ -9,11 +11,11 @@ p.yaxis.minor_tick_line_color=None
 # remove ticks on yaxis
 p.ygrid[0].ticker.desired_num_ticks=1
 # add a hover tool
-hover = HoverTool(tooltips=[("Start: ","@Start"),("End: ", "@End")])
+hover = HoverTool(tooltips=[("Start","@Start"),("End","@End")])
 p.add_tools(hover)
 
 # plot a quadrant chart
-q = p.quad(left=df["Start"], right=df["End"],bottom=0, top=1, color="orange")
+q = p.quad(left="Start", right="End",bottom=0, top=1, color="orange",source=cds)
 
 
 
