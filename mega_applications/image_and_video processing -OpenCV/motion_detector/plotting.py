f6f07c1ff2979c5d800d448ddf73665c3ab8bd6c
4bic <robyne.kiplangat@gmail.com>
2017-10-04 13:12:06 +0300

    plot a quadrant chart

--- mega_applications/image_and_video processing -OpenCV/motion_detector/plotting.py
+++ mega_applications/image_and_video processing -OpenCV/motion_detector/plotting.py
@@ -1,4 +1,11 @@
 from motion_detector import df #import data
+from bokeh.plotting import figure, output_file, show
 
-df 
-print df.head()
+# create figure object
+p=figure(x_axis_type='datetime', height=100, width=500, responsive=True, title="Motion Graph")
+# plot a quadrant chart
+q = p.quad(left=df["Start"], right=df["End"],bottom=0, top=1, color="blue")
+
+output_file("graph.html",mode='inline')
+
+show(p)
