345917446f9340973409617d72f41a414e99a818
4bic <robyne.kiplangat@gmail.com>
2017-10-04 13:30:00 +0300

    add hover tool

--- mega_applications/image_and_video processing -OpenCV/motion_detector/plotting.py
+++ mega_applications/image_and_video processing -OpenCV/motion_detector/plotting.py
@@ -1,5 +1,6 @@
 from motion_detector import df #import data
 from bokeh.plotting import figure, output_file, show
+from bokeh.models import HoverTool
 
 # create figure object
 p=figure(x_axis_type='datetime', height=100, width=500, responsive=True, title="Motion Graph")
@@ -7,11 +8,15 @@ p=figure(x_axis_type='datetime', height=100, width=500, responsive=True, title="
 p.yaxis.minor_tick_line_color=None
 # remove ticks on yaxis
 p.ygrid[0].ticker.desired_num_ticks=1
+# add a hover tool
+hover = HoverTool(tooltips=[("Start: ","@Start"),("End: ", "@End")])
+p.add_tools(hover)
+
 # plot a quadrant chart
 q = p.quad(left=df["Start"], right=df["End"],bottom=0, top=1, color="orange")
 
 
 
-output_file("motion_graph.html",mode='inline')
+output_file("graph_2.html",mode='inline')
 
 show(p)
